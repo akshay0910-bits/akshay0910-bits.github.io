dc2d58fb4363511e2ab483a9e8fe9c7e7e40fe3f
Akshay Kumar <akshayk@zenoti.com>
2018-09-24 17:44:19 +0530

    First commit

---
+++ js/plugins.js
@@ -0,0 +1,26 @@
+// Avoid `console` errors in browsers that lack a console.
+var a =function() {
+  var method;
+  var noop = function () {};
+  var methods = [
+    'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+    'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+    'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+    'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+  ];
+  var length = methods.length;
+  var console = (window.console = window.console || {});
+
+  while (length--) {
+    method = methods[length];
+
+    // Only stub undefined methods.
+    if (!console[method]) {
+      console[method] = noop;
+    }
+  }
+};
+
+var b = 1;
+
+// Place any jQuery/helper plugins in here.
